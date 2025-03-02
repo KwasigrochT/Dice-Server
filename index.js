7859c78485fb6b9c5e708ff59791c1b5f3d98781
KwasigrochT <158532345+KwasigrochT@users.noreply.github.com>
2025-03-02 14:24:04 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const cors = require("cors")
+
+app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors({origin: '*'}))
+app.use(express.json())
+
+function rollDie() {
+    return Math.floor(Math.random() * 6) +1
+}
+
+app.get("/roll-dice", (req, res) => {
+    let dice = []
+
+    for (let i = 0; i<5; i++) {
+        let roll = rollDie()
+        dice.push("You rollded " + roll)
+    }
+
+    res.json({dice})
+})
+
+app.get ("/wake-up", (req, res) =>{
+    res.json({message: "Server is awake"})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+app.use((request, response) => {
+    response.type('text/plain')
+    response.status(404)
+    response.send('404 - Not Found')
+})
